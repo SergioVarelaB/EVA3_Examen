aa398c61f80e660993a095329539598d1f161447
aaron2421 <Beetoo1313@gmail.com>
2019-12-01 11:52:18 -0700

    Ya implementada interfaz del reproductor con funcionalidad en boton de play, sig y anterior, así como animación en botón de play al botón de pausa

--- app/src/main/java/com/example/eva3_examen/MyService.java
+++ app/src/main/java/com/example/eva3_examen/MyService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,28 +15,32 @@ public class MyService extends Service {
     TextView nombre, autor;
     int cancion = 0;
     MainActivity mn = new MainActivity();
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
     @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
-        cancion = intent.getIntExtra("pos",-1);
-        if(cancion == mn.rolas.length){
+        cancion = intent.getIntExtra("pos", -1);
+        if (cancion == mn.rolas.length) {
             cancion = 0;
         }
-        Toast.makeText(getApplicationContext(),cancion+"",Toast.LENGTH_LONG).show();
+        //Toast.makeText(getApplicationContext(), cancion + "", Toast.LENGTH_LONG).show();
         mPlayer = MediaPlayer.create(getApplicationContext(), mn.rolas[cancion].getRola());
         if (mPlayer != null) {
             mPlayer.start();
         }
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/example/eva3_examen/Reproductor.java
+++ app/src/main/java/com/example/eva3_examen/Reproductor.java
@@ -3,17 +3,22 @@ package com.example.eva3_examen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Reproductor extends AppCompatActivity {
-    Intent inRola,intento;
+    Intent inRola, intento;
     TextView nombre, autor;
     int pos = 0, rola, rolaSig;
     MainActivity ma = new MainActivity();
+    ImageView imgViewPausa, imgViewPlay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,37 +27,73 @@ public class Reproductor extends AppCompatActivity {
         intento = getIntent();
         String name = intento.getStringExtra("nombre");
         String aut = intento.getStringExtra("autor");
-        rola = intento.getIntExtra("rola",-1);
-        pos = intento.getIntExtra("pos",-1);
-        rolaSig = intento.getIntExtra("rolaSig",-1);
-        TextView nombre = findViewById(R.id.tvName);
-        TextView autor = findViewById(R.id.tvAutor);
+        rola = intento.getIntExtra("rola", -1);
+        pos = intento.getIntExtra("pos", -1);
+        rolaSig = intento.getIntExtra("rolaSig", -1);
+        nombre = findViewById(R.id.tvName);
+        autor = findViewById(R.id.tvAutor);
         nombre.setText(name);
         autor.setText(aut);
-        Toast.makeText(this,pos+" ",Toast.LENGTH_LONG).show();
-        //inRola.getIntExtra();
+        //Toast.makeText(this, pos + " ", Toast.LENGTH_LONG).show();
+        imgViewPausa = findViewById(R.id.imgViewPausa);
+        imgViewPlay = findViewById(R.id.imgViewPlay);
+
     }
-    public void dale(View v){
+
+    public void dale(View v) {
         /*Intent in = new Intent();
         in.putExtra("rola",R.raw.november_rain);*/
-        inRola.putExtra("pos",pos);
-        startService(inRola);
+        //inRola.putExtra("pos", pos);
+        //startService(inRola);
+
+        if (imgViewPausa.getAlpha() == 0) {
+            inRola.putExtra("pos", pos);
+            startService(inRola);
+            //imgViewPausa.setClickable(true);
+            //imgViewPlay.setClickable(false);
+            imgViewPlay.animate().alpha(0).setDuration(1000);
+            imgViewPausa.animate().alpha(1).setDuration(1000);
+        } else if (imgViewPlay.getAlpha() == 0) {
+            inRola.putExtra("pos", pos);
+            stopService(inRola);
+            //imgViewPlay.setClickable(true);
+            //imgViewPausa.setClickable(false);
+            imgViewPausa.animate().alpha(0).setDuration(1000);
+            imgViewPlay.animate().alpha(1).setDuration(1000);
+
+        }
     }
-    public void tate(View v){
+
+    public void tate(View v) {
         stopService(inRola);
     }
-    public void siguiente(View v){
+
+    public void siguiente(View v) {
         stopService(inRola);
-        if(pos == ma.rolas.length){
+        if (pos == ma.rolas.length) {
             pos = 0;
         }
-        pos = pos+1;
+        pos = pos + 1;
         //nombre.setText("hola");
         //autor.setText(ma.rolas[pos].getAutor());
-        inRola.putExtra("pos",pos);
+        inRola.putExtra("pos", pos);
         startService(inRola);
     }
-    public void jajas(){
+
+    public void anterior(View v) {
+        stopService(inRola);
+        if (pos == ma.rolas.length) {
+            pos = 0;
+        }
+        pos = pos - 1;
+        //nombre.setText("hola");
+        //autor.setText(ma.rolas[pos].getAutor());
+        inRola.putExtra("pos", pos);
+        startService(inRola);
+    }
+
+
+    public void jajas() {
         autor.setText("jajas");
     }
 }
